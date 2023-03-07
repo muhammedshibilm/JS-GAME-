7d1136927b0e2d785c76754b73c711bd09826d87
muhammedshibilm <muhammedshibilm321@gmail.com>
2023-03-07 22:25:06 +0530

    staring page

---
+++ main.js
@@ -0,0 +1,16 @@
+var c = document.getElementById('c');
+
+
+
+//input value 
+
+
+function d() {
+var f = c.value;
+localStorage.setItem("name",f);
+
+  window.location.href = "/index.html";
+
+
+
+}
